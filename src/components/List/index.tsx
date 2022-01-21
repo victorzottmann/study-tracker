f62be18792545dd04da0baedd6c5b4bf8070787a
Victor Zottmann <zottmann.victor@gmail.com>
2022-01-21 16:14:11 +1100

    add state management for adding a new task

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,28 +1,36 @@
+import React, { useState } from 'react';
+
 import style from './List.module.scss';
 import ListItem from './ListItem';
 
 const List = () => {
-  const tasks = [
-    {
-      title: 'React',
-      time: '02:00:00'
-    }, 
-    {
-      title: 'Javascript',
-      time: '01:00:00'
-    }, 
-    {
-      title: "Typescript",
-      time: "03:00:00"
-    }
-  ];
+  const [tasks, setTasks] = useState(
+    [
+      {
+        title: 'React',
+        time: '02:00:00'
+      }, 
+      {
+        title: 'Javascript',
+        time: '01:00:00'
+      }, 
+      {
+        title: "Typescript",
+        time: "03:00:00"
+      }
+    ]
+  );
 
   return (
     <aside className={style.taskList}>
-      <h2> Today's tasks </h2>
+      <h2 onClick={() => {
+        // add the new task (inside {}) to all the other ones [...tasks] => spread operator
+        setTasks([...tasks, { title: "Study states", time: "05:00:00" }])
+      }}> Today's tasks </h2>
       <ul>
         {tasks.map((item, index) => (
           // {...item} gets all props passed into the ListItem component
+          // title and time in this case
           <ListItem key={index} {...item} />
         ))}
       </ul>
